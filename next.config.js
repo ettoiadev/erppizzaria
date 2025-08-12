5d5588f7a034bcc0ccd2c6d30cfe2179a828d869
ettoiadev <ettoiadev@gmail.com>
2025-08-12 06:36:39 -0300

    deploy no GitHub

--- next.config.js
+++ next.config.js
@@ -18,6 +18,27 @@ const nextConfig = {
   images: {
     domains: ['fonts.googleapis.com', 'fonts.gstatic.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ]
+  },
+  serverRuntimeConfig: {
+    api: {
+      bodyParser: {
+        sizeLimit: '1mb',
+      },
+      externalResolver: true,
+      responseLimit: false,
+    },
+  },
 }
 
 module.exports = nextConfig
