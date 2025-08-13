5232877d07b79e01baac5bb9c7d8a5fba55dc36a
ettoiadev <ettoiadev@gmail.com>
2025-08-13 19:28:09 -0300

    Remocao Arquivos Legados

--- app/api/simple-test/route.ts
+++
@@ -1,8 +0,0 @@
-// Rota super simples para teste
-export async function GET() {
-  return Response.json({ message: "GET working" })
-}
-
-export async function POST() {
-  return Response.json({ message: "POST working" })
-} 
